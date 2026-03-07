0b88f4db1085ea7f3771f1717c11162fa4ac7a6e
mkkbr <sercanqwertzu@gmail.com>
2026-03-07 12:39:27 +0000

    another test

--- src/router/index.js
+++ src/router/index.js
@@ -2,5 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const routes = [{ path: '/', component: HomeView }];
-const router = createRouter({ history: createWebHistory(), routes });
+const router = createRouter({
+  history: createWebHistory('/mhsheets/'), // Add this base path
+  routes,
+});
 export default router;
