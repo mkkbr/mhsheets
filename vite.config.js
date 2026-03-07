4cc20509bb5b98e0e30d226c24999b24fe51ed2c
mkkbr <sercanqwertzu@gmail.com>
2026-03-07 11:58:56 +0000

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mhsheets/',
   plugins: [vue(),tailwindcss()],
-  server: {
-    host: '0.0.0.0', // This allows access from any IP on the local network
-    port: 3000, // Change to your preferred port
-  },
+  //server: {
+  //  host: '0.0.0.0', // This allows access from any IP on the local network
+  //  port: 3000, // Change to your preferred port
+  //},
 })
