e15276d3bd5b1b093fa0ce2154391af341becb60
Sercan Simsek <si@munzinger.de>
2026-03-10 09:49:43 +0100

    routing updates

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,29 @@ import MonsterView from '../views/MonsterView.vue';
 
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/:monsterName', component: MonsterView },
+  { 
+    path: '/:monsterName', 
+    component: MonsterView,
+    beforeEnter: async (to, from, next) => {
+      try {
+        const response = await fetch('./data/wilds_monsters.json');
+        const monsters = await response.json();
+        const monsterName = to.params.monsterName;
+        
+        if (monsters.some(m => m.defname === monsterName)) {
+          next();
+        } else {
+          next('/'); 
+        }
+      } catch (error) {
+        console.error('Error fetching monster names:', error);
+        next('/');
+      }
+    }
+  },
   {
     path: '/:catchAll(.*)', // Catch-all route for 404
-    redirect: '/', component: HomeView // Redirect to home
+    redirect: '/', // Redirect to home
   },
 ];
 const router = createRouter({
