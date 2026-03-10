8df04ffe051bde8a0d9a44f962eb69f0dc0e0a1e
Sercan Simsek <si@munzinger.de>
2026-03-10 09:15:00 +0100

    Kinsect V1

--- src/components/MonsterCard.vue
+++ src/components/MonsterCard.vue
@@ -13,17 +13,17 @@
       <!-- Weapons -->
       <div class="flex justify-center gap-2">
         <div v-for="(value, weapon) in monster.weapon" :key="weapon" class="flex justify-center items-center ">
-          <img :src="`./images/icons/${weapon}.png`" :alt="weapon" class="w-10 p-1" />
+          <img :src="`./images/icons/${weapon}.png`" :alt="weapon" class="w-9 p-1" />
           <div class="flex"><span v-for="star in stars(value, 'elements')" :key="star" class="text-xs text-ctp-yellow"><i class="fa-solid fa-star"></i></span>
           </div>
         </div>
       </div>
-      <div className="flex justify-center pt-2 gap-4">
+      <div className="flex justify-center p-2 gap-4">
         <!-- Elemental Weaknesses  -->
-        <div class="flex flex-col min-w-32">
+        <div class="flex flex-col min-w-30">
           <div v-for="(value, element) in monster.elements" :key="element" class="flex items-center">
-            <img v-if="best(value, monster.elements) != 0" :src="`./images/icons/${element}.png`" :alt="element" class="w-10 mr-2 p-1 drop-shadow-[0_0px_6px_rgba(249,226,175,1)]" />
-            <img v-else :src="`./images/icons/${element}.png`" :alt="element" class="w-10 mr-2 p-1" />
+            <img v-if="best(value, monster.elements) != 0" :src="`./images/icons/${element}.png`" :alt="element" class="w-9 mr-2 p-1 drop-shadow-[0_0px_6px_rgba(249,226,175,1)]" />
+            <img v-else :src="`./images/icons/${element}.png`" :alt="element" class="w-9 mr-2 p-1" />
             <div class="flex">
               <span v-if="stars(value, 'elements') == 0" class="text-base text-ctp-red"><i class="fa-solid fa-xmark"></i></span>
               <span v-for="star in stars(value, 'elements')" :key="star" class="text-base text-ctp-yellow"><i class="fa-solid fa-star"></i></span>
@@ -31,19 +31,19 @@
           </div>
         </div>
         <!-- Status Weaknesses -->
-        <div class="flex flex-col min-w-32">
+        <div class="flex flex-col min-w-30">
           <div v-for="(value, status) in monster.status" :key="status" class="flex items-center">
-            <img :src="`./images/icons/${status}.png`" :alt="status" class="w-10 mr-2 p-1" />
+            <img :src="`./images/icons/${status}.png`" :alt="status" class="w-9 mr-2 p-1" />
             <div class="flex">
               <span v-if="stars(value, 'status') == 0" class="text-base text-ctp-red"><i class="fa-solid fa-xmark"></i></span>
-              <span v-for="star in stars(value, 'status')" :key="star" class="text-base text-ctp-yellow-700"><i class="fa-solid fa-star"></i></span>
+              <span v-for="star in stars(value, 'status')" :key="star" class="text-base text-ctp-yellow-800"><i class="fa-solid fa-star"></i></span>
             </div>
           </div>
         </div>
       </div>
-      <div class="gap-4 flex justify-center p-2 text-sm">
-        <span v-if="monster.kinsect.red != '0'"  class="text-ctp-red flex">          
-        <img src="/images/icons/kinsect.png" alt="kinsect" title="kinsect" class="w-6 h-6 mr-4" />{{ monster.kinsect.red}}</span>
+      <div class="gap-4 flex justify-center p-2 text-sm items-center">
+        <span v-if="monster.kinsect.red != '0'"  class="text-ctp-red flex items-center">          
+        <img src="/images/icons/kinsect.png" alt="kinsect" title="kinsect" class="w-7 mr-4" />{{ monster.kinsect.red}}</span>
         <span v-if="monster.kinsect.orange != '0'" class="text-ctp-peach">{{ monster.kinsect.orange }}</span>
         <span v-if="monster.kinsect.white != '0'" class="text-ctp-text">{{ monster.kinsect.white }}</span>
         <span v-if="monster.kinsect.green != '0'" class="text-ctp-green">{{ monster.kinsect.green }}</span>
@@ -51,19 +51,19 @@
       <!-- Misc Items (Bottom Row) -->
       <div class="flex justify-center p-1">
         <img v-if="monster.misc.severable" src="/images/icons/sword.png" alt="Severable" title="Severable Parts"
-          class="w-8 h-8" />
+          class="w-8" />
         <img v-if="monster.misc.flash" src="/images/icons/flash_pod.png" alt="Flash Pod" title="Flash Pod"
-          class="w-8 h-8" />
+          class="w-8" />
         <img v-if="monster.misc.sonic" src="/images/icons/sonic_pod.png" alt="Sonic Pod" title="Sonic Pod"
-          class="w-8 h-8" />
+          class="w-8" />
         <img v-if="monster.misc.shock" src="/images/icons/shock_trap.png" alt="Shock Trap" title="Shock Trap"
-          class="w-8 h-8" />
+          class="w-8" />
         <img v-if="monster.misc.pitfall" src="/images/icons/pitfall_trap.png" alt="Pitfall Trap" title="Pitfall Trap"
-          class="w-8 h-8" />
+          class="w-8" />
         <img v-if="monster.misc.vine" src="/images/icons/vine_trap.png" alt="Vine Trap" title="Vine Trap"
-          class="w-8 h-8" />
+          class="w-8" />
         <img v-if="monster.misc.dung" src="/images/icons/dung_pod.png" alt="Dung Pod" title="Dung Pod"
-          class="w-8 h-8" />
+          class="w-8" />
       </div>
     </div>
   </div>
