cdf4931759a055cef399352d96adf92ac2c4873a
Sercan Simsek <si@munzinger.de>
2026-03-10 09:40:26 +0100

    catch 404 - return to home

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,10 @@ import MonsterView from '../views/MonsterView.vue';
 const routes = [
   { path: '/', component: HomeView },
   { path: '/:monsterName', component: MonsterView },
+  {
+    path: '/:catchAll(.*)', // Catch-all route for 404
+    redirect: '/', component: HomeView // Redirect to home
+  },
 ];
 const router = createRouter({
   history: createWebHistory('/mhsheets/'),
