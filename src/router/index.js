900ab2c224979cc4323cc72cb32d07bf5fb36aff
Sercan Simsek <si@munzinger.de>
2026-03-10 11:11:47 +0100

    back on empty page added

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import MonsterView from '../views/MonsterView.vue';
 
 
 const routes = [
   { path: '/', component: HomeView },
-  { 
-    path: '/:monsterName', 
-    component: MonsterView,
-    beforeEnter: async (to, from, next) => {
-      try {
-        const response = await fetch('./data/wilds_monsters.json');
-        const monsters = await response.json();
-        const monsterName = to.params.monsterName;
-        
-        if (monsters.some(m => m.defname === monsterName)) {
-          next();
-        } else {
-          next('/'); 
-        }
-      } catch (error) {
-        console.error('Error fetching monster names:', error);
-        next('/');
-      }
-    }
-  },
+  { path: '/:monsterName', component: MonsterView},
   {
     path: '/:catchAll(.*)', // Catch-all route for 404
     redirect: '/', // Redirect to home
   },
 ];
 const router = createRouter({
-  history: createWebHistory('/mhsheets/'),
+  history: createWebHashHistory('/mhsheets/'),
   routes,
 });
 
--- src/views/MonsterView.vue
+++ src/views/MonsterView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="min-h-screen p-4 bg-ctp-crust text-ctp-teal">
     <!-- Header -->
-    <header  @click="openGrid"  class="pb-2 flex items-center justify-center relative max-w-3xl mx-auto">
-        <button class="hover:text-ctp-yellow text-2xl flex items-center gap-1 absolute left-0">
+    <header @click="openGrid" class="pb-2 flex items-center justify-center relative max-w-3xl mx-auto">
+      <button class="hover:text-ctp-yellow text-2xl flex items-center gap-1 absolute left-0">
         <i class="fa-solid fa-angles-left"></i>
       </button>
       <h1 class="text-2xl hover:text-ctp-yellow">Monster Hunter Wilds</h1>
@@ -12,13 +12,28 @@
     <div v-if="monster" class="max-w-lg mx-auto mb-4 md:max-w-3xl">
       <MonsterCard :monster="monster" />
     </div>
+    <div v-else class="p-20 flex items-center justify-center">
+      <div class="relative inline-flex  group ">
+        <div
+          class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r 
+          from-[#89dceb] via-[#74c7ec] to-[#fab387] rounded-xl blur-lg group-hover:opacity-100 
+          group-hover:-inset-1 group-hover:duration-200 animate-pulse">
+        </div>
+        <a href="#" title="back"
+          class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold 
+          text-ctp-text text-xl transition-all duration-200 bg-ctp-base rounded-md focus:outline-none 
+          focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+          role="button"><i class="fa-solid fa-angles-left"></i>BACK
+        </a>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted, watch } from 'vue';
-import { useRoute, useRouter  } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import MonsterCard from '../components/MonsterCard.vue';
 
 const route = useRoute();
